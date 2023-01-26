5c44d3f3c316025271070edda4ac9da5b472a1e7
Codes-of-Fabric <codefabric3@gmail.com>
2023-01-26 15:59:13 -0600

    What is a for loop
    Added new code

---
+++ Python Basics/What is  a For Loop?.py
@@ -0,0 +1,34 @@
+numbers = [3, 4, 5, 6, 7, 8]
+names = ['Adam', 'Andrew', 'Alex', 'Steve']
+
+for number in numbers:
+     print(number)
+
+for number in numbers[:3]:
+     print(number)
+
+for number in numbers[::-1]:
+     print(number)
+
+for number in range(len(numbers)):
+     print(number)
+     
+for name in names:
+        if name == 'Steve':
+            continue
+        print(name)
+     
+     
+
+    
+
+       
+    
+    
+    
+    
+    
+    
+    
+    
+        
