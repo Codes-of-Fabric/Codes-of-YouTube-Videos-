a418f46c5a3834b70a35e777c662713734ae16b7
Codes-of-Fabric <codefabric3@gmail.com>
2023-02-06 18:42:20 -0600

    Added what is slicing code

---
+++ Python Basics/What is Slicing?.py
@@ -0,0 +1,19 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+name = 'Mount Everest is the tallest'
+numbers_set = {11, 12, 13, 14}
+
+'''Input_sequence[start:stop:step]'''
+
+numbers[:3]
+numbers[3:]
+numbers[1::2]
+numbers[4:1:-1]
+numbers[::-1]
+name[4::-1]
+numbers_set[:3]
+
+numbers[:2] = []
+numbers
+numbers[:0] = [4, 4, 4]
+
+name[0:2] = 'NN'
